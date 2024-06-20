c687927a7b74d04ab4da49d64123b6c0eb1b9aa2
Asante Azevedo <59629941+Croissantaye@users.noreply.github.com>
2024-06-20 13:13:57 -0700

    update dactyl 5x6 trackball keymap
    This adjusts trackball sensitivity and some changes to the keymap

--- keyboards/handwired/dactyl_5x6_trackball/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_5x6_trackball/keymaps/default/keymap.c
@@ -45,27 +45,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
         KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,
         KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,
-        KC_LGUI, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                                    
+        KC_LCTL, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                                    
                         KC_HOME, KC_END, KC_ENT,KC_SPC , RAISE ,    
-                                                KC_LCTL, KC_LALT,       
+                                                KC_LGUI, KC_LALT,       
 
                          KC_6 ,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
                          KC_Y ,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL ,
                          KC_H ,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
                          KC_N ,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                         KC_DEL,             KC_LBRC,KC_RBRC,
-        KC_RALT, TG(RAISE), KC_BSPC
+                         KC_BSPC,             KC_LBRC,KC_RBRC,
+        KC_RALT, TG(RAISE), KC_DEL
     ),
 
     /**
      *  ┬───┬───┬───┬───┬───┬───┐ 
      *  │F12│ F1│ F2│ F3│ F4│ F5│ 
      *  ┼───┼───┼───┼───┼───┼───┤ 
-     *  │   │   │ V+│ V-│ Vx│   │
+     *  │   │   │ V+│ V-│ Vx│SLP│
      *  ┼───┼───┼───┼───┼───┼───┤
      *  │Sht│ ← │ ↓ │ ↑ │ → │   │
      *  ┼───┼───┼───┼───┼───┼───┤
-     *  │SLP│   │Nxt│p/p│Pre│   │
+     *  │   │   │Nxt│p/p│Pre│   │
      *  ┴───┴───┼───┼───┼───┼───┼───┐
      *          │PgU│PgD│Mod│   │   │
      *          └───┴───┴───┼───┼───┤
@@ -90,9 +90,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_ergodox(
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,
-        _______ , _______ ,KC_VOLD,KC_VOLU,KC_MUTE, _______ ,
+        _______ , _______ ,KC_VOLD,KC_VOLU,KC_MUTE, KC_SLEP ,
         _______, KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT, _______  ,
-        KC_SLEP, _______ , KC_MNXT,KC_MPLY,KC_MPRV, _______  ,
+        _______, _______ , KC_MNXT,KC_MPLY,KC_MPRV, _______  ,
                         KC_PGUP, KC_PGDN,
                                          _______ ,_______ , _______ ,
                                                 _______, _______,
