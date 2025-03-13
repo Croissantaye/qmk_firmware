33cd7ac9d66afcbe4da429da32b563b1ed1a59a1
croissantaye <asa81099@gmail.com>
2025-03-13 11:46:33 -0700

    update dactyl 5x6 trackball keymap

--- keyboards/handwired/dactyl_5x6_trackball/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_5x6_trackball/keymaps/default/keymap.c
@@ -12,10 +12,10 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * 
-     *  ┬───┬───┬───┬───┬───┬───┐ 
-     *  │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │ 
-     *  ┼───┼───┼───┼───┼───┼───┤ 
+     *
+     *  ┬───┬───┬───┬───┬───┬───┐
+     *  │Esc│ 1 │ 2 │ 3 │ 4 │ 5 │
+     *  ┼───┼───┼───┼───┼───┼───┤
      *  │Tab│ Q │ W │ E │ R │ T │
      *  ┼───┼───┼───┼───┼───┼───┤
      *  │Shf│ A │ S │ D │ F │ G │
@@ -26,10 +26,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *          └───┴───┴───┼───┼───┤
      *                      │Gui│Ctl│
      *                      └───┴───┘
-     * 
-     *          ┬───┬───┬───┬───┬───┬───┐ 
-     *          │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ 
-     *          ┼───┼───┼───┼───┼───┼───┤ 
+     *
+     *          ┬───┬───┬───┬───┬───┬───┐
+     *          │ 6 │ 7 │ 8 │ 9 │ 0 │ - │
+     *          ┼───┼───┼───┼───┼───┼───┤
      *          │ Y │ U │ I │ O │ P │ = │
      *          ┼───┼───┼───┼───┼───┼───┤
      *          │ H │ J │ K │ L │ : │ ' │
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,
         KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,
         KC_LSFT, KC_A  , KC_S  , KC_D  , KC_F  , KC_G  ,
-        RAISE,   KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                                    
-                        KC_HOME, KC_END,KC_BSPC,KC_DEL , KC_RALT ,    
-                                                KC_LGUI, KC_LCTL,       
+        RAISE,   KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,
+                        KC_HOME, KC_END,KC_BSPC,KC_DEL , KC_RALT ,
+                                                KC_LGUI, KC_LCTL,
 
                          KC_6 ,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
                          KC_Y ,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL ,
@@ -58,9 +58,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     /**
-     *  ┬───┬───┬───┬───┬───┬───┐ 
-     *  │F12│ F1│ F2│ F3│ F4│ F5│ 
-     *  ┼───┼───┼───┼───┼───┼───┤ 
+     *  ┬───┬───┬───┬───┬───┬───┐
+     *  │F12│ F1│ F2│ F3│ F4│ F5│
+     *  ┼───┼───┼───┼───┼───┼───┤
      *  │   │   │ ↑ │   │   │SLP│
      *  ┼───┼───┼───┼───┼───┼───┤
      *  │Sht│ ← │ ↓ │ → │   │V+ │
@@ -71,10 +71,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *          └───┴───┴───┼───┼───┤
      *                      │   │   │
      *                      └───┴───┘
-     * 
-     *          ┬───┬───┬───┬───┬───┬───┐ 
-     *          │ F6│ F7│ F8│ F9│F10│F11│ 
-     *          ┼───┼───┼───┼───┼───┼───┤ 
+     *
+     *          ┬───┬───┬───┬───┬───┬───┐
+     *          │ F6│ F7│ F8│ F9│F10│F11│
+     *          ┼───┼───┼───┼───┼───┼───┤
      *          │   │MWU│MWD│MWL│MWR│   │
      *          ┼───┼───┼───┼───┼───┼───┤
      *          │   │LMB│RMB│MW │BCK│FWD│
@@ -85,12 +85,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *  ┌───┬───┼───┤   └───┴───┘
      *  │   │   │   │
      *  └───┴───┴───┘
-     * 
+     *
      */
 
     [_RAISE] = LAYOUT_ergodox(
         KC_F12 , KC_F1 , KC_F2 , KC_F3 , KC_F4 , KC_F5 ,
-        _______ , _______ ,KC_UP,       ,_______, KC_SLEP ,
+        _______ , _______ ,KC_UP,_______,_______, KC_SLEP ,
         _______, KC_LEFT,KC_DOWN,KC_UP  ,KC_RGHT, KC_VOLU  ,
         _______, _______ , KC_MNXT,KC_MPLY,KC_MPRV, KC_VOLD  ,
                         KC_PGUP, KC_PGDN,
